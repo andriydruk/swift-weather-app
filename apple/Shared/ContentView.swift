b1bdf3ab20746724063a829add441a75e3076fcd
Andrius Shiaulis <shiaulis@gmail.com>
2020-07-04 17:12:57 +0300

    Add new xcode project

---
+++ apple/Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Andrius Shiaulis on 04.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apple/Shared/SwiftWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWeatherApp.swift
+//  Shared
+//
+//  Created by Andrius Shiaulis on 04.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
