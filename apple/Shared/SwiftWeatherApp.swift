1544b693c5ece59f2de3f1d81e2fb0ec53a7193e
Andrew Druk <adruk@readdle.com>
2020-07-15 20:25:19 +0300

    Dev: add simple location search

--- apple/Shared/SwiftWeatherApp.swift
+++ apple/Shared/SwiftWeatherApp.swift
@@ -6,12 +6,27 @@
 //
 
 import SwiftUI
+import WeatherCore
 
 @main
 struct SwiftWeatherApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+    
+    init() {
+        // Example of WeatherCore usage
+        let provider = MetaWeatherProvider()
+        provider.searchLocations(query: "San") { locations, error in
+            if let error = error {
+                NSLog("Error: %s", error.localizedDescription)
+                return
+            }
+            locations?.forEach {
+                NSLog($0.title)
+            }
         }
     }
 }
