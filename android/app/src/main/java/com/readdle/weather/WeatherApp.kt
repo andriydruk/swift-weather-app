cb990464a02a692fdb722cf2b789c50b08f70942
Olof Thorén <OlofT@users.noreply.github.com>
2021-08-26 09:25:37 +0200

    Load Swift in a separate thread
    Give Swift its own thread to get DispatchQueue.main.async, etc working.

--- android/app/src/main/java/com/readdle/weather/WeatherApp.kt
+++ android/app/src/main/java/com/readdle/weather/WeatherApp.kt
@@ -5,16 +5,25 @@ import com.readdle.codegen.anotation.JavaSwift
 import com.readdle.weather.core.SSLHelper
 import com.readdle.weather.utils.copyAssetsIfNeeded
 import dagger.hilt.android.HiltAndroidApp
+import kotlin.concurrent.thread
 
 @HiltAndroidApp
 class WeatherApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        System.loadLibrary("WeatherCoreBridge")
-        JavaSwift.init()
-        assets.copyAssetsIfNeeded("cacert.pem", dataDir.absolutePath)
-        SSLHelper.setupCert(dataDir.absolutePath)
+        var sleep = true
+        thread {
+            //this gives Swift a mainThread of its own (for calling DispatchQueue.main.async etc)
+            System.loadLibrary("WeatherCoreBridge")
+            JavaSwift.init()
+            assets.copyAssetsIfNeeded("cacert.pem", dataDir.absolutePath)
+            SSLHelper.setupCert(dataDir.absolutePath)
+            sleep = false
+        }
+        while (sleep) {
+            Log.d("breezeApp", "sleeping")
+        }
     }
 
 }
